8ca0759cedf670c2273ccfdc774b86875c173959
Troy Caselli <troycaselli@Troys-MacBook-Air.local>
2022-11-25 13:32:09 -0800

    loosly hooked up server, cars-router, and cars-model, and created error endpoints

--- api/cars/cars-router.js
+++ api/cars/cars-router.js
@@ -1,1 +1,24 @@
-// DO YOUR MAGIC
+const express = require('express');
+const Cars = require('./cars-model');
+
+const router = express.Router();
+
+router.get('/', async (req, res) => {
+    const result = await Cars.getAll();
+    res.status(200).json(result);
+})
+
+// router.get('/:id', (req, res) => {
+
+// })
+
+// router.post('/', (req, res) => {
+
+// })
+
+router.use((error, req, res, next) => {
+    res.status(error.status || 500).json({
+        message: error.message || 'Something went wrong'
+    })
+})
+module.exports = router;
--- api/server.js
+++ api/server.js
@@ -1,7 +1,14 @@
 const express = require("express")
+const carsRouter = require('./cars/cars-router')
 
 const server = express()
 
-// DO YOUR MAGIC
+server.use(express.json())
 
-module.exports = server
+server.use('/api/cars', carsRouter)
+
+server.use('*', (req, res) => {
+    res.status(404).json({message: `${req.method} ${req.baseUrl} not found`});
+})
+
+module.exports = server;
